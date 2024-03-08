a577aa2b2e0b4916619dbca7d2e1d99acc0dc2ed
Maurits van Rees <maurits@vanrees.org>
2024-03-08 13:21:02 +0100

    Back to development: 1.0.0b4
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = f"""
 
 setup(
     name="plone.distribution",
-    version="1.0.0b3",
+    version="1.0.0b4.dev0",
     description="Plone distribution support",
     long_description=long_description,
     long_description_content_type="text/markdown",
