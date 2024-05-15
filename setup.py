3d690b2d0bdb7546156dc73312780671b408b6e7
Érico Andrei <ericof@gmail.com>
2024-05-16 01:34:57 +0200

    Back to development: 2.0.0a2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = f"""
 
 setup(
     name="plone.distribution",
-    version="2.0.0a1",
+    version="2.0.0a2.dev0",
     description="Plone distribution support",
     long_description=long_description,
     long_description_content_type="text/markdown",
