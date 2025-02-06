3f6b0504dce206c670eeedc067d03d8739ee0554
Maurits van Rees <maurits@vanrees.org>
2025-02-06 19:03:30 +0100

    Bumped version for feature release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = f"""
 
 setup(
     name="plone.distribution",
-    version="3.0.1.dev0",
+    version="3.1.0.dev0",
     description="Plone distribution support",
     long_description=long_description,
     long_description_content_type="text/markdown",
