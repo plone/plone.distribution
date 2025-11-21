e09ae664be2121f9680e69e2991d5b6ee07e6ca0
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-21 23:35:39 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = f"""
 
 setup(
     name="plone.distribution",
-    version="3.2.2.dev0",
+    version="4.0.0.dev0",
     description="Plone distribution support",
     long_description=long_description,
     long_description_content_type="text/markdown",
