edf4f4ccf21c25b8673f8ef3b1175b40a56d8194
Steve Piercy <web@stevepiercy.com>
2025-11-28 22:07:08 -0800

    Remove path and related import

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,7 @@
 from importlib.metadata import version
 
 import datetime
-import os
 import sys
-
-
-sys.path.insert(0, os.path.abspath("../src/"))
 
 
 # -- Project information -----------------------------------------------------
